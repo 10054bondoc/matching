16516d5cd02524fb39fe764495560fafb2ea5b2b
cesar bondoc <10054bondoc@gmail.com>
2020-07-28 13:15:41 +0800

    corrected key

--- src/BoardGame.js
+++ src/BoardGame.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./BoardGame.module.css";
 import Tile from "./Tile";
 import images from "./imagelist";
@@ -23,20 +23,11 @@ const numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 shuffle(numArray);
 
 export default function BoardGame() {
-  const [count, setCount] = useState(0);
-
-  function adder() {
-    setCount((prev) => {
-      return prev + 1;
-    });
-    console.log(count);
-  }
-
   return (
     <div className={classes.BoardGame}>
       {numArray.map((el) => {
         return (
-          <div key={el.toString()} onClick={adder}>
+          <div key={el.toString()}>
             <Tile imgsource={images.source[el]} />
           </div>
         );
